c6f492cce30873febab2191e4d76dd3552c08b84
ebowwa <ec.arbee1@gmail.com>
2024-05-06 00:23:26 -0700

    brunt

--- src/lib/blog/api.ts
+++ src/lib/blog/api.ts
@@ -1,7 +1,7 @@
 // src/lib/blog/api.ts
 
 import { z } from 'zod';
-import rawData from '../../../public/raw-data/posts.json';
+import rawData from '@public/raw_data/posts.json';
 import PostType from '@/interfaces/post';
 
 // Zod schema for individual post
