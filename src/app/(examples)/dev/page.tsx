ac50ca047468aa1adf226bf1fa1f259ee0f5f2fc
ebowwa <ec.arbee1@gmail.com>
2024-05-09 11:54:42 -0700

    k

--- src/app/(examples)/dev/page.tsx
+++ src/app/(examples)/dev/page.tsx
@@ -1,12 +1,10 @@
 "use client"
 // app/page.tsx
 import HexColorPicker from '../../../components/(dev-tools)/hex';
-import GithubToJsDelivr from '../../../components/(dev-tools)/GithubLinktoJsDelivr';
 
 export default function Home() {
   return (
     <main>
-      <GithubToJsDelivr />
       <HexColorPicker />
     </main>
   );
