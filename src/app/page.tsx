8d3917c3ea87a28b22e6160f54295330f447785a
ebowwa <ec.arbee1@gmail.com>
2024-05-08 19:40:28 -0700

    add contact and some movement in component paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 // app/page.tsx
 "use client";
-import LandingContainer from '@/components/landing/sections/General_Landing_Container'; // <LandingContainer />
-import PerplexitySearch from '@/components/landing/sections/perplexity-search'; // <PerplexitySearch />
-import InfoSubstrate from '@/components/landing/sections/InfoSubstrate'; // <InfoSubstrate />
-import SiteMap, { SiteMapProps, SiteMapItem } from '@/components/landing/sections/SiteMap';
+import LandingContainer from '@/components/landing/sections/general/General_Landing_Container'; // <LandingContainer />
+import PerplexitySearch from '@/components/landing/sections/general/perplexity-search'; // <PerplexitySearch />
+import InfoSubstrate from '@/components/landing/sections/general/InfoSubstrate'; // <InfoSubstrate />
+import SiteMap, { SiteMapProps, SiteMapItem } from '@/components/landing/sections/general/SiteMap';
 import siteMapData from './sitemap.json';
 
 export default function HomePage() {
