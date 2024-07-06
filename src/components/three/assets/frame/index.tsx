c98581c38a483a8ae71011e81ce95fcfac21f390
Ebowwa <ec.arbee1@gmail.com>
2024-07-05 22:01:44 -0700

    better image frame

--- src/components/three/assets/frame/index.tsx
+++ src/components/three/assets/frame/index.tsx
@@ -1,21 +1,21 @@
 // src/components/three/assets/frame/index.tsx
 "use client"
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { PictureFrame } from '@/components/three/assets/frame/Picture_frame'
-import { OrbitControls, Environment } from '@react-three/drei'
-import tunnel from 'tunnel-rat'
-import ImagePlane from './ImagePlane'
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { PictureFrame } from '@/components/three/assets/frame/Picture_frame';
+import { OrbitControls, Environment } from '@react-three/drei';
+import tunnel from 'tunnel-rat';
+import ImagePlane from './ImagePlane';
 
 // Import the tunnel-rat library for UI interactions
-const t = tunnel()
+const t = tunnel();
 
 // Define the props for the ImageDisplayComponent
 interface ImageDisplayComponentProps {
   // The source of the image to be displayed
-  imageSource: string
+  imageSource: string;
   // A flag to determine whether the image should be displayed
-  showImage: boolean
+  showImage: boolean;
 }
 
 // The ImageDisplayComponent is a React functional component
@@ -25,30 +25,30 @@ const ImageDisplayComponent: React.FC<ImageDisplayComponentProps> = ({ imageSour
     <div className="w-screen h-screen">
       {/* Render the Three.js Canvas component */}
       <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas>
-        {/* Add an ambient light to the scene */}
-        <ambientLight intensity={6.5} />
-        {/* Add a point light to the scene */}
-        <pointLight position={[10, 10, 10]} intensity={1} />
-        {/* Add a spot light to the scene */}
-        <spotLight position={[10, 10, 10]} angle={0} penumbra={1} intensity={2} castShadow />
-        {/* Render the PictureFrame component */}
-        <PictureFrame />
-        {/* Add OrbitControls to the scene, allowing the user to navigate the camera */}
-        <OrbitControls />
-        {/* Set the environment preset to "city" */}
-        <Environment preset="city" />
+        <Canvas>
+          {/* Add an ambient light to the scene */}
+          <ambientLight intensity={6.5} />
+          {/* Add a point light to the scene */}
+          <pointLight position={[10, 10, 10]} intensity={1} />
+          {/* Add a spot light to the scene */}
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} intensity={2} castShadow />
+          {/* Render the PictureFrame component */}
+          <PictureFrame />
+          {/* Add OrbitControls to the scene, allowing the user to navigate the camera */}
+          <OrbitControls enableDamping dampingFactor={0.25} rotateSpeed={0.5} />
+          {/* Set the environment preset to "city" */}
+          <Environment preset="city" />
 
-        {/* If the showImage prop is true, render the ImagePlane component with the provided imageSource */}
-        {showImage && <ImagePlane imageSource={imageSource} />}
-      </Canvas>
-    </div>
+          {/* If the showImage prop is true, render the ImagePlane component with the provided imageSource */}
+          {showImage && <ImagePlane imageSource={imageSource} />}
+        </Canvas>
+      </div>
       {/* Render the UI elements using the tunnel-rat library */}
       <div id="ui">
         <t.Out />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageDisplayComponent
+export default ImageDisplayComponent;
