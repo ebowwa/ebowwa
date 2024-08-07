ebd18516d10cb9b9c3349fe301d2a573cf2f937f
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-08-07 01:05:58 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // app/page.tsx
 import React from 'react';
+import Link from 'next/link';
 
 const VideoPage = () => {
   return (
@@ -13,7 +14,7 @@ const VideoPage = () => {
           muted
           playsInline
         >
-          <source src="https://cdn.jsdelivr.net/gh/ebowwar/asset-store@main/ebowwa-xyz/F4FD3EC2-2198-45C2-B4AD-35B538622A8F.mov" type="video/quicktime" />
+          <source src="https://cdn.jsdelivr.net/gh/ebowwar/asset-store@main/ebowwa-xyz/0720.mov" type="video/quicktime" />
           Your browser does not support the video tag.
         </video>
       </div>
@@ -24,14 +25,16 @@ const VideoPage = () => {
       {/* Content */}
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center z-10 text-center px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 lg:mb-8 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600">
-          Coming Soon
+          New & Coming Soon
         </h1>
         <p className="text-base sm:text-lg lg:text-xl mb-6 sm:mb-8 max-w-xl sm:max-w-2xl mx-auto">
-          Get ready for an immersive experience that will redefine your expectations.
+          accelerate.
         </p>
-        <button className="px-4 sm:px-6 py-2 sm:py-3 bg-gradient-to-r from-blue-500 to-purple-600 rounded-full text-sm sm:text-base font-semibold hover:from-blue-600 hover:to-purple-700 transition duration-300">
-          Stay Tuned
-        </button>
+        <Link href="https://simulationguide.vercel.app/elijah/">
+          <button className="px-4 sm:px-6 py-2 sm:py-3 bg-gradient-to-r from-blue-500 to-purple-600 rounded-full text-sm sm:text-base font-semibold hover:from-blue-600 hover:to-purple-700 transition duration-300">
+            Visit Old
+          </button>
+        </Link>
       </div>
 
       {/* Futuristic elements - adjusted for better visibility */}
