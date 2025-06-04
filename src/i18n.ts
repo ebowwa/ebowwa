ce54c56588ab9dbbbda8bcb29cd82640b33ebeca
ebowwa <81942069+ebowwa@users.noreply.github.com>
2025-06-03 18:13:16 -0700

    Add next-intl config and adjust middleware

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,4 @@
-export type Locale = (typeof locales)[number];
-
 export const locales = ['en', 'es'] as const;
+export type Locale = (typeof locales)[number];
 export const defaultLocale: Locale = 'en';
+export const localePrefix = 'as-needed';
